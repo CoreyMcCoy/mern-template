a153a1ec2745704f7e9d803f78e28fabc0cd6540
Corey McCoy <coreyamccoy@gmail.com>
2023-01-27 21:38:24 -0600

    Added Register, Login and Logout functionality

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
